42de67a5fb4c681e1b3c12d8f5b25dff3f56ac7f
Won D <fkiller@gmail.com>
2016-08-21 07:14:23 -0400

    new cities added

--- mongodbscripts.js
+++ mongodbscripts.js
@@ -4,3 +4,16 @@ var createLoc = function() {
  db.pokestop.save(doc);
  });
  };
+
+db.pokestop.find({loc:{$nearSphere: {$geomerty:{type:"Point",coordinates:[-73.9667,40.78]},$maxDistance:5000}}}).count();
+var dbscan = {
+    Eps:1000,
+    MinPts:5,
+    dbscanmap : function() {
+    emit(db.pokestop.find({loc:{$nearSphere: {$geometry:this.loc}}}).sort({loc:1}).limit(1).toArray()[0];}
+};
+
+};
+var 
+
+db.pokestop.find({loc:{$nearSphere: {$geometry:{type:"Point",coordinates:[-73.9667,40.78]},$maxDistance:1000}}}).sort({loc:1}).limit(1);
